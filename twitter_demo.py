a753ae2b79048e43bdc198d58b87bb5817d092c7
longxiaofei <libra.19951002@gmail.com>
2023-10-19 11:55:23 +0800

    feat: update twitter demo

--- twitter_demo.py
+++ twitter_demo.py
@@ -1,6 +1,7 @@
 from typing import Tuple
-from datetime import datetime, timedelta
+from datetime import datetime
 
+import duckdb
 import pandas as pd
 import streamlit as st
 from streamlit.elements.widgets.time_widgets import DateWidgetReturn
@@ -16,11 +17,15 @@ st.title("Streamlit Twitter Demo")
 # Initialize pygwalker communication
 init_streamlit_comm()
 
+@st.cache_data
+def get_twitter_df() -> "pd.DataFrame":
+    df = pd.read_json(st.secrets["DATA_URL"])
+    return df[list(set(df.columns) - {"edit_controls", "public_metrics", "referenced_tweets", "context_annotations"})]
+
 # When using `use_kernel_calc=True`, you should cache your pygwalker renderer, if you don't want your memory to explode
 @st.cache_resource
 def get_pyg_renderer() -> "StreamlitRenderer":
-    df = pd.read_json("./formated_twitter_datas.json")
-    df = df[list(set(df.columns) - {"edit_controls", "public_metrics", "referenced_tweets", "context_annotations"})]
+    df = get_twitter_df()
     # When you need to publish your application, you need set `debug=False`,prevent other users to write your config file.
     return StreamlitRenderer(df, spec="./twitter_gw_config.json", debug=False)
 
@@ -30,9 +35,37 @@ renderer = get_pyg_renderer()
 # display explore ui
 # renderer.render_explore()
 
+
+def display_metrics_gather(start_date: "DateWidgetReturn", end_date: "DateWidgetReturn"):
+    df = get_twitter_df()
+    metrics = duckdb.query(f"""
+        SELECT
+            COUNT(1) twiiters_count,
+            SUM(impression_count) impression_count,
+            SUM(like_count) like_count,
+            SUM(retweet_count) retweet_count,
+            SUM(reply_count) reply_count,
+            SUM(quote_count) quote_count,
+            SUM(bookmark_count) bookmark_count
+        FROM
+            df
+        WHERE
+            created_at BETWEEN '{start_date.strftime("%Y-%m-%d")}' AND '{end_date.strftime("%Y-%m-%d")}'
+    """).fetchone()
+    metrics_cols = st.columns(7)
+    metrics_names = [
+        "twiiters_count", "impression_count", "like_count", "retweet_count",
+        "reply_count", "quote_count", "bookmark_count"
+    ]
+    for metrics_col, metrics_name, metrics_value in zip(metrics_cols, metrics_names, metrics):
+        metrics_col.metric(metrics_name, metrics_value)
+
+    renderer.render_pure_chart(10)    
+
+
 def display_date_picker() -> Tuple["DateWidgetReturn", "DateWidgetReturn"]:
-    default_end_date = datetime.now()
-    default_start_date = default_end_date - timedelta(days=60)
+    default_end_date = datetime(2023, 10, 19)
+    default_start_date = datetime(2023, 8, 19)
     col1, col2, _ = st.columns([1, 1, 6])
     with col1:
         start_date = st.date_input("start time: ", default_start_date.date())
@@ -92,6 +125,9 @@ pre_filters.append(PreFilter(
 ))
 
 renderer.set_global_pre_filters(pre_filters)
+
+st.subheader("metrics gather")
+display_metrics_gather(start_date, end_date)
 
 st.subheader("metrics by day")
 display_metrics_charts()
